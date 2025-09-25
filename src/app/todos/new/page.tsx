598152256daa72ff1ac99fd9b70d5d959e5e3b1d
teesmile <anthonyugwujac@gmail.com>
2025-09-25 22:06:11 +0100

    modified the ui and some functionality

---
+++ src/app/todos/new/page.tsx
@@ -0,0 +1,9 @@
+export default function NewTodoPage() {
+  return (
+    <div>
+      <h1>New Todo Page</h1>
+      <p>Form to create a new todo will go here</p>
+    </div>
+  )
+}
+ 
---
+++ src/app/todos/page.tsx
@@ -0,0 +1,8 @@
+export default function HomeTodosPage() {
+  return (
+    <div>
+      <h1>Todos Page</h1>
+      <p>List of todos will go here</p>
+    </div>
+  )
+}
